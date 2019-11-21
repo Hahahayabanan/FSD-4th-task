ae5e746d730e271d85cfdedd6f690c5513dc0039
Hahahayabanan <olegvolobuev97@mail.ru>
2019-11-21 11:23:32 +0400

    Deleted .d.ts files #9

--- src/EventObserver/EventObserver.d.ts
+++
@@ -1,9 +0,0 @@
-declare class EventObserver {
-    private observers;
-    constructor();
-    subscribe(fn: Function): void;
-    unsubscribe(fn: Function): void;
-    broadcast(data: any): void;
-}
-export { EventObserver };
-export default EventObserver;
