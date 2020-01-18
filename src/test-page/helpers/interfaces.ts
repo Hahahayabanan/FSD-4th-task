41680abe91607208844ee23ff6027a927dcd0c5b
Hahahayabanan <olegvolobuev97@mail.ru>
2020-01-19 02:41:34 +0400

    Removed particle background

--- src/test-page/helpers/interfaces.ts
+++ src/test-page/helpers/interfaces.ts
@@ -1,24 +1,5 @@
 import InputTextField from '../blocks/input-text-field/input-text-field';
 
-export interface ParticleProperties {
-  bgWidth?: number;
-  bgHeight?: number;
-  bgColor?: string;
-  color?: string;
-  radiusModifier?: number;
-  radius?: number;
-  count?: number;
-  velocityModifier?: number;
-  velocityX?: number;
-  velocityY?: number;
-  x?: number;
-  y?: number;
-  shadowBlur?: number;
-  shadowModifier?: number;
-  alpha?: number;
-  alphaModifier?: number;
-}
-
 export type TextFieldsArray = {
   [key: string] : InputTextField;
 };
